09e764f44e33493c10346fe1530950f3e68ba960
Calvin962 <117885528+Calvin962@users.noreply.github.com>
2022-11-16 14:02:04 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,82 @@
+basic.showString("Guess The Shape!")
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . # . # .
+    . # # # .
+    `)
+basic.pause(5000)
+basic.showString("HOUSE")
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
+basic.pause(5000)
+basic.showString("TREE")
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
+basic.pause(5000)
+basic.showString("CIRCLE")
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showString("SQUARE")
+basic.showLeds(`
+    # . . . .
+    # # . . .
+    # . # . .
+    # . . # .
+    # # # # #
+    `)
+basic.pause(5000)
+basic.showString("TRIANGLE")
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.pause(5000)
+basic.showString("SMILEY FACE")
+basic.showLeds(`
+    . . # . .
+    . # . . .
+    . # # # .
+    . . # . .
+    . # . . .
+    `)
+basic.pause(5000)
+basic.showString("LIGHTNING")
+basic.showLeds(`
+    . # . # .
+    # . # . #
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+basic.pause(5000)
+basic.showString("HEART")
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+basic.pause(5000)
+basic.showString("DIAMOND")
